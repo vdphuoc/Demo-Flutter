ecd9dc9fd8d3d776b5825375304e0f08d3512446
Gnasche <phuoc.workspace@gmail.com>
2023-10-13 01:15:31 +0700

    add login screen and assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 //adding comment test
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
@@ -25,6 +29,108 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Image logoImg = Image.asset('images/loginImg.jpg');
+
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.blue,
+              Colors.green
+            ], // Replace with your gradient colors
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                // height: MediaQuery.of(context).size.height / 2,
+                // width: MediaQuery.of(context).size.width / 2,
+                height: 200,
+                width: 200,
+
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromARGB(255, 149, 220, 17), // Border color
+                    width: 0.2, // Border width
+                  ),
+                  borderRadius:
+                      BorderRadius.circular(25.0), // To make it a circle
+                ),
+                child: Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(25.0),
+                    child: Image.asset(
+                      'images/loginImg.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'DEMO.',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Username',
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle login logic here
+                      },
+                      child: Text('Login'),
+                    ),
+                    SizedBox(height: 10),
+                    TextButton(
+                      onPressed: () {
+                        // Handle signup navigation here
+                      },
+                      child: Text('Sign Up'),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    'Developed by Phuoc',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
